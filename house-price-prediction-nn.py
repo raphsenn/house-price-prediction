1d254210f4c0174cfd5e84fb6aad1e318ae633c5
raphsenn <raphaelsenn@outlook.de>
2024-04-01 10:22:40 +0200

    update

--- house-price-prediction-nn.py
+++ house-price-prediction-nn.py
@@ -4,71 +4,16 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-"""
-price,area,bedrooms,bathrooms,stories,mainroad,guestroom,basement,
-hotwaterheating,airconditioning,parking,prefarea,furnishingstatus
-"""
-
-
-def read_labled_data(file: str) -> tuple[np.array, np.array]:
-    """
-    >>> X, y = read_labled_data('housing-doctest.csv')
-    >>> X
-    array([[7.42e+03, 4.00e+00, 2.00e+00, 3.00e+00, 1.00e+00, 0.00e+00,
-            0.00e+00, 0.00e+00, 1.00e+00, 2.00e+00, 1.00e+00, 1.00e+00],
-           [8.96e+03, 4.00e+00, 4.00e+00, 4.00e+00, 1.00e+00, 0.00e+00,
-            0.00e+00, 0.00e+00, 1.00e+00, 3.00e+00, 0.00e+00, 1.00e+00],
-           [9.96e+03, 3.00e+00, 2.00e+00, 2.00e+00, 1.00e+00, 0.00e+00,
-            1.00e+00, 0.00e+00, 0.00e+00, 2.00e+00, 1.00e+00, 5.00e-01]])
-    >>> y
-    array([13300000, 12250000, 12250000])
-    """ 
-    X = [] # Inputs (area, bedrooms, bathrooms...).
-    prices = []
-    
-    # Mapping dictionary for categorical features
-    feature_mapping = {'yes': 1, 'no': 0, 'furnished': 1, 'semi-furnished': 0, 'unfurnished': -1}
-     
-    with open(file, 'r') as file:
-        csvreader = csv.reader(file)
-        # Skip first line.
-        next(csvreader)
-        
-        for row in csvreader:
-            price = int(row[0])
-            features = [float(feature_mapping[row[i]]) if row[i] in feature_mapping else float(row[i]) for i in range(1, len(row))]
-            X.append(features)
-            prices.append(price)
-        X = np.array(X)
-        y = np.array(prices)
-        return X, y
-
-def read_data(file: str) -> tuple[np.array, np.array]:
-    boston = pd.read_csv(file, header=None, delimiter=r"\s+", names=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV'])
-    X = boston.iloc[:,:-1]
-    y = boston.iloc[:, -1]
-    X = np.array(X)
-    y = np.array(y)
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X) 
-    return X, y
-
-
-def sigmoid(x: np.array) -> np.array:
-    return 1 / (1 + np.exp(-x))
-
-def ReLu(x):
-    return np.maximum(0, x)
-
 class NeuralNetwork:
     def __init__(self, input_size: int=12) -> None:
-        """
-        """ 
         self.w1 = np.zeros((input_size, 4))
         self.b1 = np.zeros(4)
 
         self.w2 = np.zeros((4, 1))
         self.b2 = np.zeros(1)
+
+    def sigmoid(self, x: np.array) -> np.array:
+        return 1.0 / (1.0 + np.exp(-x))
 
     def train(self, X: np.array, y: np.array, epochs: int=10, learning_rate: float=0.01, verbose: bool=False) -> None:
         """
@@ -76,7 +21,7 @@ class NeuralNetwork:
         for epoch in range(epochs):
             # Forward propagation.
             Z1 = np.dot(X, self.w1) + self.b1
-            A1 = sigmoid(Z1)
+            A1 = self.sigmoid(Z1)
             Z2 = np.dot(A1, self.w2) + self.b2
             A2 = Z2 # Predictions.
             
@@ -101,7 +46,7 @@ class NeuralNetwork:
     def predict(self, X: np.array) -> int:
         # Forward propagation.
         Z1 = np.dot(X, self.w1) + self.b1
-        A1 = sigmoid(Z1)
+        A1 = self.sigmoid(Z1)
         Z2 = np.dot(A1, self.w2) + self.b2
         A2 = Z2 
         return A2.ravel()
@@ -112,10 +57,49 @@ class NeuralNetwork:
         mse = np.mean((predictions - y) ** 2)
         return mse
 
+def read_labled_data(file: str) -> tuple[np.array, np.array]:
+    X = [] # Inputs (area, bedrooms, bathrooms...).
+    prices = []
+    
+    # Mapping dictionary for categorical features
+    feature_mapping = {'yes': 1, 'no': 0, 'furnished': 1, 'semi-furnished': 0, 'unfurnished': -1}
+     
+    with open(file, 'r') as file:
+        csvreader = csv.reader(file)
+        # Skip first line.
+        next(csvreader)
+        
+        for row in csvreader:
+            row = row[0].strip('[]').split()
+            price = float(row[13])
+            # price = int(row[0])
+            features = [float(feature_mapping[row[i]]) if row[i] in feature_mapping else float(row[i]) for i in range(0, len(row) -1)]
+            X.append(features)
+            prices.append(price)
+        X = np.array(X)
+        scaler = StandardScaler() 
+        X = scaler.fit_transform(X) 
+        y = np.array(prices)
+        return X, y
+
+def read_data(file: str) -> tuple[np.array, np.array]:
+    boston = pd.read_csv(file, header=None, delimiter=r"\s+", names=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV'])
+    X = boston.iloc[:,:-1]
+    y = boston.iloc[:, -1]
+    X = np.array(X)
+    y = np.array(y)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X) 
+    return X, y
+
 
 if __name__ == '__main__':
-    X_train, y_train = read_data('housing2.csv')
+    # X_train, y_train = read_data('housing2.csv')
+    # X_train, y_train = read_labled_data('Housing.csv')
+    X_train, y_train = read_labled_data('housing2.csv')
+    print(X_train)
     nn = NeuralNetwork(13)
     nn.train(X_train, y_train, 5000, 0.001, True)
+    
     loss = nn.evaluate(X_train, y_train)
     print(loss)
